7abce7f9c1cf3c71b101a79efec14b5c6104bbd9
kyle456 <kyle2123@naver.com>
2020-12-01 17:32:41 +0900

    test: 메뉴 선택 시 1, 2 이외 값 예외 발생 테스트 코드 추가

--- src/main/java/encryption/MenuSelectValidator.java
+++ src/main/java/encryption/MenuSelectValidator.java
@@ -5,7 +5,7 @@ public class MenuSelectValidator {
     private static final String LOG_IN = "2";
 
     public static void validateMenuSelectIsOneOrTwo(String menuSelect) {
-        if (menuSelect.equals(SIGN_UP) || menuSelect.equals(LOG_IN)) {
+        if (!menuSelect.equals(SIGN_UP) && !menuSelect.equals(LOG_IN)) {
             throw new IllegalArgumentException("1 또는 2만 입력해야 합니다.(1.회원가입 / 2.로그인)");
         }
     }
---
+++ src/test/java/encryption/MenuSelectValidatorTest.java
@@ -0,0 +1,25 @@
+package encryption;
+
+import org.junit.Test;
+
+public class MenuSelectValidatorTest {   
+    @Test(expected = IllegalArgumentException.class)
+    public void 다른숫자_입력시_예외발생() {
+        MenuSelectValidator.validateMenuSelectIsOneOrTwo("3");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void 문자_입력시_예외발생() {
+        MenuSelectValidator.validateMenuSelectIsOneOrTwo("+");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void 공백_입력시_예외발생() {
+        MenuSelectValidator.validateMenuSelectIsOneOrTwo(" ");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void 줄바꿈_입력시_예외발생() {
+        MenuSelectValidator.validateMenuSelectIsOneOrTwo("\\n");
+    }
+}
