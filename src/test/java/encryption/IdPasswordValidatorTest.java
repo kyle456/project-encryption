fc8fb03d18553ca88e630a4329e95950d0c65e36
kyle456 <kyle2123@naver.com>
2020-12-01 17:39:47 +0900

    test: id와 password에 공백, 줄바꿈 예외발생 테스트 코드 추가

---
+++ src/test/java/encryption/IdPasswordValidatorTest.java
@@ -0,0 +1,28 @@
+package encryption;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IdPasswordValidatorTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void 패스워드_공백_예외발생() {
+        IdPasswordValidator.checkIdPasswordContainsSpace("id", "pass word");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void 아이디_공백_예외발생() {
+        IdPasswordValidator.checkIdPasswordContainsSpace("i d", "password");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void 패스워드_줄바꿈_예외발생() {
+        IdPasswordValidator.checkIdPasswordContainsSpace("id", "password\\n");
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void 아이디_줄바꿈_예외발생() {
+        IdPasswordValidator.checkIdPasswordContainsSpace("id\\n", "password");
+    }
+}
