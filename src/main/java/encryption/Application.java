42cc618afad9a0c6ef281b7e22e84315ca04b153
kyle456 <kyle2123@naver.com>
2020-12-01 19:05:33 +0900

    feat(Application): 회원 가입 기능 추가
    1. 중복 회원 가입 방지를 위해 ID 중복 여부 기능 추가
    2. ID, Password 반영구 저장을 위해 userInfo.txt 파일에 정보 저장 기능 추가

--- src/main/java/encryption/Application.java
+++ src/main/java/encryption/Application.java
@@ -1,43 +1,69 @@
 package encryption;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         final Scanner scanner = new Scanner(System.in);
-        final InputView inputview = new InputView(scanner);
+        final InputView inputView = new InputView(scanner);
+        while (true) {
+            String selection = selectMenu(inputView);
+            if (selection.equals("1")) {
+                signUp(inputView);
+            }
+            if (selection.equals("2")) {
+                logIn(inputView);
+            }
+        }
+    }
 
+    public static String selectMenu(InputView inputView) {
         String menuSelect;
+        try {
+            menuSelect = inputView.printMenuSelectView();
+            MenuSelectValidator.validateMenuSelectIsOneOrTwo(menuSelect);
+            return menuSelect;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return selectMenu(inputView);
+        }
+    }
+
+    public static void signUp(InputView inputView) throws IOException {
         String id;
         String password;
-        
-        // 메뉴선택
         try {
-            menuSelect = inputview.printMenuSelectView();
-            MenuSelectValidator.validateMenuSelectIsOneOrTwo(menuSelect);
+            id = inputView.printIdInput();
+            password = inputView.printPasswordInput();
+            IdPasswordValidator.validateSignUp(id, password);
+            saveToUserInfoTxt(id, password);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            // 반복 추가
+            signUp(inputView);
         }
-        
-        // 회원가입
+    }
+
+    public static void saveToUserInfoTxt(String id, String password) throws IOException {
+        PrintWriter printWriter = new PrintWriter(new FileWriter(
+                "C:\\Users\\YOUNGJOO\\Desktop\\project_encryption\\src\\main\\resources\\userInfo.txt", true)); // true 시 추가 쓰기
+        String userInfo = id + "," + password;
+        printWriter.println(userInfo);
+        printWriter.close();
+    }
+
+    public static void logIn(InputView inputView) {
+        String id;
+        String password;
         try {
-            id = inputview.printIdInput();
-            password = inputview.printPasswordInput();
-            IdPasswordValidator.validateSignUp(id, password);;
+            id = inputView.printIdInput();
+            password = inputView.printPasswordInput();
+            IdPasswordValidator.validateLogIn(id, password);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            // 반복 추가
-        }
-        
-        // 로그인
-        try {
-            id = inputview.printIdInput();
-            password = inputview.printPasswordInput();
-            IdPasswordValidator.validateLogIn(id, password);;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            // 반복 추가
+            logIn(inputView);
         }
     }
 }
--- src/main/java/encryption/IdPasswordValidator.java
+++ src/main/java/encryption/IdPasswordValidator.java
@@ -1,19 +1,44 @@
 package encryption;
 
-public class IdPasswordValidator {   
-    public static void validateSignUp(String id, String password) {
-        checkIdPasswordContainsSpace(id, password);
-        // 이미 존재하는 id, password 일 경우 회원가입 예외처리 추가
-    }
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class IdPasswordValidator {
+    private static final int ID = 0;
     
+    public static void validateSignUp(String id, String password) throws IOException {
+        checkIdPasswordContainsSpace(id, password);
+        checkIdDuplication(id);
+    }
+
     public static void validateLogIn(String id, String password) {
         checkIdPasswordContainsSpace(id, password);
         // 존재하지 않는 id, password 일 경우 로그인 예외처리 추가
     }
-    
+
     public static void checkIdPasswordContainsSpace(String id, String password) {
         if (id.contains(" ") || id.contains("\\n")) {
             throw new IllegalArgumentException("ID 또는 Password에 공백이 입력되었습니다. 다시 입력해주세요.");
         }
+    }
+
+    public static void checkIdDuplication(String id) throws IOException {
+        FileReader userInfo = new FileReader(
+                "C:\\Users\\YOUNGJOO\\Desktop\\project_encryption\\src\\main\\resources\\userInfo.txt");
+        BufferedReader bufferedReader = new BufferedReader(userInfo);
+        String line;
+        String existedId;
+        while (true) {
+            line = bufferedReader.readLine();
+            if (line == null) {
+                break;
+            }
+            existedId = line.split(",")[ID];
+            if (id.equals(existedId)) {
+                throw new IllegalArgumentException("이미 생성된 계정입니다.");
+            }
+        }
+        bufferedReader.close();
     }
 }
